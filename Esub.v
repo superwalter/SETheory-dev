3807b4f0c6d29082d4340def23f7b98648defb5f
superwalter <qw04ster@gmail.com>
2012-11-07 22:10:05 +0800

    Half done Esub

--- Esub.v
+++ Esub.v
@@ -20,7 +20,7 @@ Definition eq_esub (es es' : esub):=
     (eq_val i i' -> eq_val (es.(esubi) i) (es'.(esubi) i')) /\
     (Lc.eq_intt j j' -> Lc.eq_intt (es.(esubj) j) (es'.(esubj) j')).
 
-Definition esub_conv (es : esub) (i : val) (j : Lc.intt) : (val * Lc.intt) := 
+Definition esub_conv (es : esub) (i : val) (j : Lc.intt): (val * Lc.intt) := 
   ((es.(esubi) i), (es.(esubj) j)).
 
 Definition eq_val_int (x y :(val * Lc.intt)):= eq_val (fst x) (fst y) /\ 
@@ -32,51 +32,65 @@ do 5 red; simpl; intros. subst y.
 destruct x; simpl. rewrite H0. rewrite H1. split; reflexivity.
 Qed.
 
-Definition app_esub : esub_i -> esub_j -> trm -> trm.
-intros es_i es_j t; left.
-exists (fun i => int (esub_conv_i es_i i) t) (fun j => tm (esub_conv_j es_j j) t).
+Definition esub_conv_i (es : esub) (i : val) : val := (es.(esubi) i).
+
+Instance esub_conv_i_morph : 
+  Proper (eq ==> eq_val ==> eq_val) esub_conv_i.
+do 6 red; simpl; intros. subst y.
+destruct x; simpl. apply esubi_morph0; trivial.
+Qed.
+
+Definition esub_conv_j (es : esub) (j : Lc.intt) : Lc.intt := (es.(esubj) j).
+
+Instance esub_conv_j_morph : 
+  Proper (eq ==> Lc.eq_intt ==> Lc.eq_intt) esub_conv_j.
+do 5 red; simpl; intros. subst y.
+destruct x; simpl. apply esubj_morph0; trivial.
+Qed.
+
+Definition app_esub : esub -> trm -> trm.
+intros es t; left.
+exists (fun i => int (esub_conv_i es i) t) (fun j => tm (esub_conv_j es j) t).
 do 4 red; intros. apply int_morph; try reflexivity.
  apply esub_conv_i_morph; trivial.
 
 do 2 red; intros. apply tm_morph; try reflexivity.
  apply esub_conv_j_morph; trivial.
 
-destruct es_j as (f, (H, (Hl, Hs))); simpl.
+destruct es as (f, Hmrphf, g, Hmrphg, Hl, Hs); simpl.
 red; intros. rewrite Hl.
 destruct t; [destruct i; simpl; apply itm_lift0|]; trivial.
 
-destruct es_j as (f, (H, (Hl, Hs))); simpl.
+destruct es as (f, Hmrphf, g, Hmrphg, Hl, Hs); simpl.
 red; intros. rewrite Hs.
 destruct t; [destruct i; simpl; apply itm_subst0|]; trivial.
 Defined.
 
-Definition id_sub_i : esub_i.
-exists (fun f => f). do 2 red; trivial.
+Definition id_sub : esub.
+exists (fun f => f) (fun f => (fun _ => Lc.K)).
+do 2 red; trivial.
+
+do 4 red; trivial.
+
+do 2 red; trivial.
+
+do 2 red; trivial.
 Defined.
 
-Definition id_sub_j : esub_j.
-exists (fun f => (fun _ => Lc.K)). 
-split; [do 4 red|split; do 2 red; intros]; trivial.
-Defined.
-
-Definition sub_cons_i : trm -> esub_i -> esub_i.
-intros t es.
-exists (fun i => V.cons (int i t) (esub_conv_i es i)).
+Definition sub_cons : trm -> esub -> esub.
+intros t es. 
+exists (fun i => V.cons (int i t) (esub_conv_i es i))
+  (fun j => I.cons (tm j t) (esub_conv_j es j)).
 do 2 red; intros. apply V.cons_morph; 
   [rewrite H; reflexivity| apply esub_conv_i_morph; trivial].
-Defined.
 
-Definition sub_cons_j : trm -> esub_j -> esub_j.
-intros t es.
-exists (fun j => I.cons (tm j t) (esub_conv_j es j)).
-split; [|split]; intros.
- do 4 red; intros; rewrite H; trivial.
+do 4 red; intros; rewrite H; trivial.
 
- do 2 red; intros. destruct es as (f, (H, (Hl, Hs))); simpl. rewrite Hl. 
- destruct t; [destruct i; simpl; rewrite itm_lift0|]; destruct a; simpl; trivial.
+do 2 red; intros. destruct es as (f, Hmrphf, g, Hmrphg, Hl, Hs); simpl. rewrite Hl. 
+destruct t; [destruct i; simpl; rewrite itm_lift0|]; destruct a; simpl; trivial.
 
- do 2 red; intros. destruct es as (f, (H, (Hl, Hs))); simpl. rewrite Hs. 
- destruct t; [destruct i; simpl; rewrite itm_subst0|]; destruct a; simpl; trivial.
+do 2 red; intros. destruct es as (f, Hmrphf, g, Hmrphg, Hl, Hs); simpl. rewrite Hs. 
+destruct t; [destruct i; simpl; rewrite itm_subst0|]; destruct a; simpl; trivial.
 Defined.
 
 Definition typ_esub env1 es_i es_j env2 := 
